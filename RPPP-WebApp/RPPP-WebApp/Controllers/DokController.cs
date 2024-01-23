0750a6f013c8d5a25600b26ed9d46ac032750f7e
Bruno Mikulan <bruno.mikulan@fer.hr>
2024-01-23 12:04:23 +0100

    dokumentacija za dokument controllere

--- RPPP-WebApp/RPPP-WebApp/Controllers/DokController.cs
+++ RPPP-WebApp/RPPP-WebApp/Controllers/DokController.cs
@@ -7,12 +7,22 @@ using RPPP_WebApp.Models;
 using System.Collections.Generic;
 using System.Linq;
 
+
+/// <summary>
+/// pomocni kontroller za rad s dokumentima(CRUD controller, ali ne radi jTable na frontend)
+/// </summary>
 public class DokController : Controller
 {
     private readonly RPPP05Context ctx;
     private readonly ILogger<DokController> logger;
     private readonly AppSettings appSettings;
 
+    /// <summary>
+    /// inicijalizacija kontrollera
+    /// </summary>
+    /// <param name="ctx">contekst, za nadovezivanje na bazu</param>
+    /// <param name="options">postavke aplikacije</param>
+    /// <param name="logger">logger za biljezenj dogadaja</param>
     public DokController(RPPP05Context ctx, IOptionsSnapshot<AppSettings> options, ILogger<DokController> logger)
     {
         this.ctx = ctx;
@@ -22,12 +32,21 @@ public class DokController : Controller
 
     // API Endpoints
 
+    /// <summary>
+    /// CRUD opreacija za dohvacanje svih dokumenata
+    /// </summary>
+    /// <returns>svi dokumenti</returns>
     [HttpGet("/api/Dok")]
     public IEnumerable<Dokument> Get()
     {
         return ctx.Dokument.ToList();
     }
 
+    /// <summary>
+    /// CRUD operacija za dohvacanje dokumenta po id-u
+    /// </summary>
+    /// <param name="id">id dokumenta</param>
+    /// <returns>dokument s odgovarajucim ID</returns>
     [HttpGet("/api/Dok/{id}")]
     public ActionResult<Dokument> Get(int id)
     {
@@ -41,6 +60,11 @@ public class DokController : Controller
         return document;
     }
 
+    /// <summary>
+    /// CRUD operacija za dodavanje dokumenta
+    /// </summary>
+    /// <param name="value">dokument</param>
+    /// <returns>status dodavanja, OK - ako uspjelo, BadRequest ako nije</returns>
     [HttpPost("/api/Dok")]
     public ActionResult<Dokument> Post([FromBody] Dokument value)
     {
@@ -50,9 +74,16 @@ public class DokController : Controller
         }
         ctx.Dokument.Add(value);
         ctx.SaveChanges();
-        return NoContent();
+        return Ok();
         
     }
+
+    /// <summary>
+    /// CRUD operacija za azuriranje dokumenta
+    /// </summary>
+    /// <param name="id">id dokumenta</param>
+    /// <param name="value">dokument s azuriranim podacima</param>
+    /// <returns>status azuriranja, OK - ako uspjelo, BadRequest ako nije </returns>
 
     [HttpPut("/api/Dok/{id}")]
     public ActionResult<Dokument> Put(int id, [FromBody] Dokument value)
@@ -65,9 +96,15 @@ public class DokController : Controller
 
         ctx.Dokument.Update(value);
         ctx.SaveChanges();
-        return NoContent();
+        return Ok();
     }
 
+
+    /// <summary>
+    /// CRUD operacija za brisanje dokumenta
+    /// </summary>
+    /// <param name="id">id dokumenta namjenjenog za brisanje</param>
+    /// <returns>Status brisanja</returns>
     [HttpDelete("/api/Dok/{id}")]
     public ActionResult<Dokument> Delete(int id)
     {
@@ -81,11 +118,14 @@ public class DokController : Controller
 
         ctx.Dokument.Remove(document);
         ctx.SaveChanges();
-        return NoContent();
+        return Ok();
     }
 
     // Views
-
+    /// <summary>
+    /// stranica s JTable-om za prikaz dokumenata
+    /// </summary>
+    /// <returns>stranicu s JTable-om</returns>
     public IActionResult Index()
     {
         return View();
